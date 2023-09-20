8d676bb961c3cf36fc2053893390565846a3f284
vinh.nguyen <vinh.nguyen@service.nsw.gov.au>
2023-09-20 11:38:03 +1000

    master: add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+    createBrowserRouter,
+    RouterProvider
+} from "react-router-dom";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
+import {Contact} from "./routes/contact.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root/>,
+        errorElement: <ErrorPage/>,
+        children: [
+            {
+                path: "/contacts/:contactId",
+                element: <Contact/>
+            }
+        ]
+    },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
