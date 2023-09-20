ef9d9c2774bc71b8f512cf44a7001a4fcac53497
vinh.nguyen <vinh.nguyen@service.nsw.gov.au>
2023-09-21 00:43:24 +1000

    main: add routes

---
+++ src/routes/root.jsx
@@ -0,0 +1,57 @@
+import {Outlet, Link, useLoaderData, Form, redirect} from "react-router-dom";
+import { getContacts, createContact } from "../contacts.js";
+
+
+export const loader = async () => {
+    const contacts = await getContacts();
+    return {contacts};
+}
+
+export const action = async () => {
+    const contact = await createContact();
+    return redirect(`/contacts/${contact.id}/edit`)
+}
+
+export default function Root() {
+    return (
+        <>
+            <div id="sidebar">
+                <h1>React Router Contacts</h1>
+                <div>
+                    <form id="search-form" role="search">
+                        <input
+                            id="q"
+                            aria-label="Search contacts"
+                            placeholder="Search"
+                            type="search"
+                            name="q"
+                        />
+                        <div
+                            id="search-spinner"
+                            aria-hidden
+                            hidden={true}
+                        />
+                        <div
+                            className="sr-only"
+                            aria-live="polite"
+                        ></div>
+                    </form>
+                    <form method="post">
+                        <button type="submit">New</button>
+                    </form>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href={`/contacts/1`}>Your Name</a>
+                        </li>
+                        <li>
+                            <a href={`/contacts/2`}>Your Friend</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div id="detail"></div>
+        </>
+    );
+}
